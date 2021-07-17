c01fdd10ed057f4d4172d51c43f640107d4aed67
ss271189 <shashank2714@gmail.com>
2021-07-18 01:14:52 +0530

    Adding Contact info to Owner. Closed #28

--- pet-clinic-data/src/main/java/jc/springframework/model/Owner.java
+++ pet-clinic-data/src/main/java/jc/springframework/model/Owner.java
@@ -4,6 +4,9 @@ import java.util.Set;
 
 public class Owner extends Person {
     private Set<Pet> pet;
+    private String address;
+    private String city;
+    private String telephone;
 
     public Set<Pet> getPet() {
         return pet;
@@ -11,5 +14,29 @@ public class Owner extends Person {
 
     public void setPet(Set<Pet> pet) {
         this.pet = pet;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
     }
 }
